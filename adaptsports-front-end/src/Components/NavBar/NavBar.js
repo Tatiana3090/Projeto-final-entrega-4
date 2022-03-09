bbf9959206834320e3d026353835e8a19845cd10
Apolock33 <carlosammgomes@gmail.com>
2022-03-09 19:07:14 -0300

    NavBar Finalizado

--- adaptsports-front-end/src/Components/NavBar/NavBar.js
+++ adaptsports-front-end/src/Components/NavBar/NavBar.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import Logo from '../Logo'
 import logoImg from '../../Assets/Logos/logo azul.svg'
+import logoImg2 from '../../Assets/Logos/logo verde.svg'
 import './Navbar.css';
 
 export default function NavBar() {
   return (
     <header className='navBarApp'>
-      
       <div>
-        <Logo logo={logoImg} />
+        <a href='/'><Logo logo={logoImg} /></a>
       </div>
-
       <div>
         <ul className='list'>
-          <li><a href='/'>Home</a></li>
-          <li><a href='/sobrenos'>Sobre Nós</a></li>
-          <li><a href='/locais'>Locais</a></li>
-          <li><a href='/esportes'>Esportes</a></li>
+          <li><a className='a' href='/'>Home</a></li>
+          <li><a className='a' href='/sobrenos'>Sobre Nós</a></li>
+          <li><a className='a' href='/locais'>Locais</a></li>
+          <li><a className='a' href='/esportes'>Esportes</a></li>
+          <li><a className='Login' href='/'>LogIn</a></li>
         </ul>
       </div>
-
     </header>
   );
 }
